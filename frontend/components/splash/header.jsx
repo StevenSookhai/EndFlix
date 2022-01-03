0e07f1755415516dc375bfd2c72ff22a508c7632
Stevens <stevensookhai@gmail.com>
2022-01-03 01:00:12 -0500

    more splash styling, css why you do this ;-;

--- frontend/components/splash/header.jsx
+++ frontend/components/splash/header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     const logo = "https://fontmeme.com/permalink/220102/80b2e83ec91311621e8aea703b915905.png"
     return(
         <div className="header-container">
-            <img className="logo"src={logo} />
+            <img className="logo" src={logo} />
             <div className="sign-up-button-container">
                 <Link className="splash-login-btn" to='/login'> Sign In </Link>
             </div>
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -28,10 +28,60 @@ class Splash extends React.Component{
                     </div>
                     <div className="second-splash-section">
                                 <div className="splash-second-contents">
-                                    <h1>Enjoy on your TV.</h1>
-                                    <h2>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h2>
+                                    <h1 className="enjoy-tv">Enjoy on your TV.</h1>
+                                    <h2 className="smart-tv">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h2>
+                                </div>
+
+                                <div>
+                                    <video src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"></video>
                                 </div>
                     </div>
+                    <div className="third-splash-section">
+                        <div className="splash-third-contents">
+                            {/* <video src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"></video> */}
+                            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" alt="" />
+                        </div>
+                        <div>
+                            <h1 className="enjoy-tv">Download your shows to watch offline.</h1>
+                            <h2 className="smart-tv">Save your favorites easily and always have something to watch.</h2>
+                        </div>
+                    </div>
+
+                    <div className="fourth-splash-section">
+                        <div className="splash-fourth-contents">
+                            {/* <video src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"></video> */}
+                            <h1 className="enjoy-tv">Watch everywhere.</h1>
+                            <h2 className="smart-tv">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.</h2>
+                        </div>
+                        <div>
+                            <video src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v"></video>
+                        </div>
+                    </div>
+                    <div className="fifth-splash-section">
+                        <div className="splash-fifth-contents">
+                            {/* <video src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"></video> */}
+                            <img src="https://occ-0-448-444.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABdFTpLmANuJpYneLq8L5m7CunMCi8e8Nl4y7xaPVWzG3IeoDoq17egTQAthApKg_4sdRWdwuR8KadWu1frjL3JQImpwq.png?r=fcd" alt="" />
+                        </div>
+                        <div>
+                            <h1 className="enjoy-tv">Create profiles for kids.</h1>
+                            <h2 className="smart-tv">Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</h2>
+                        </div>
+                    </div>
+                    {/* <footer> */}
+                    <div className='splash-footer'>
+                        <p>Social links</p>
+
+                        <ul>
+                            <li>
+                                <a href="">GitHub</a>
+                            </li>
+
+                            <li>
+                                <a href="">My LinkedIn</a>
+                            </li>
+                        </ul>
+                    </div>
+                {/* </footer> */}
                 </div>
             </div>
         )
