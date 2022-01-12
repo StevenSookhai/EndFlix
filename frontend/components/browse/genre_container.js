4454cfbe62ce5d2eba039184b7a31f6d631d2674
Stevens <stevensookhai@gmail.com>
2022-01-11 22:24:09 -0500

    add to list bug fixed

--- frontend/components/browse/genre_container.js
+++ frontend/components/browse/genre_container.js
@@ -1,17 +1,25 @@
 import { connect } from "react-redux";
 import { fetchAllVideos } from "../../actions/video_actions";
+import { createListItem, fetchlists, deleteMylist } from '../../actions/mylist_actions'
+
 
 import Genre from "./genre";
 
 const MapStateToProps = (state, ownProps) => {
     return {
-        videos: Object.values(state.entities.videos)
+        video: ownProps.video,
+        videos: Object.values(state.entities.videos),
+        lists: state.entities.myList,
+        currentProfile: state.session.currentProfile
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return{
-        fetchAllVideos: videos => dispatch(fetchAllVideos(videos))
+        fetchAllVideos: videos => dispatch(fetchAllVideos(videos)),
+        createListItem: (movieId, profileId) => dispatch(createListItem(movieId, profileId)),
+        fetchlists: lists => dispatch(fetchlists(lists)),
+        deleteMylist: listId => dispatch(deleteMylist(listId))
     }
 }
 
--- frontend/components/browse/genre_list_item.jsx
+++ frontend/components/browse/genre_list_item.jsx
@@ -1,27 +1,52 @@
 import React from "react";
 import MovieShowPage from "./movie_show_page";
 import MovieShowPageContainer from "./movie_show_page_container";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchAllVideos } from "../../actions/video_actions";
+import { createListItem, fetchlists, deleteMylist } from '../../actions/mylist_actions'
 
-export default class GenreListItem extends React.Component{
+
+
+ class GenreListItem extends React.Component{
     constructor(props){
         super(props)
         this.state = {
             showModal: false,
             hovered: false,
-            playVideo: false
+            playVideo: false,
+            inlist: false
         }
         this.showModal = this.showModal.bind(this)
         this.handleHover = this.handleHover.bind(this)
         this.handleHoverExit = this.handleHoverExit.bind(this)
         this.handlePlay = this.handlePlay.bind(this)
+        this.onList = this.onList.bind(this)
+        this.handleAddToList = this.handleAddToList.bind(this)
     }
     componentDidMount(){
-        // this.props.fetchAllVideos()
+        this.props.fetchAllVideos()
+        this.props.fetchlists()
     }
     handlePlay(){
+        console.log("clicked")
         this.setState({ playVideo: true })
 
     }
+     handleAddToList() {
+         // return console.log("clicked")
+         const inList = Object.values(this.props.lists).filter(listItem => listItem.video_id === this.props.video.id);
+         if (inList.length === 0) {
+             this.setState({ inlist: true })
+             debugger
+             return this.props.createListItem(this.props.video.id, this.props.currentProfile.id)
+         } else {
+             const mylist = Object.values(this.props.lists).find(listItem => listItem['video_id'] === this.props.video.id);
+             this.setState({ inlist: false })
+             debugger
+             return this.props.deleteMylist(mylist.id)
+         }
+        }
     handleHover(){
         
         // setTimeout(() => {
@@ -37,13 +62,20 @@ export default class GenreListItem extends React.Component{
     showModal(){
         this.setState({showModal: !this.state.showModal} )
     }
+
+    onList() {
+        const inList = Object.values(this.props.lists).filter(listItem => listItem.video_id === this.props.video.id);
+        return inList.length > 0
+    }
     render(){
         if(this.props.video === undefined) return null
 
-        if (this.state.playVideo && this.props.hovered) {
+        if (this.state.playVideo ) {
             debugger
             return <Redirect to={`/watch/${this.props.video.id}`} />
         }
+
+        const text = this.onList() ? '✓' : '+'
         // debugger
         // const img = this.props.video.thumbnailURL ? this.props.video.thumbnailURL : "https://endflix-seeds.s3.amazonaws.com/tempthumbnail.jpg"
         const img = "http://www.simpleimageresizer.com/_uploads/photos/5b203fd6/sq_914x514.jpg"
@@ -55,7 +87,7 @@ export default class GenreListItem extends React.Component{
                      onMouseEnter={this.handleHover}
                      onMouseLeave={this.handleHoverExit}>
                     {/* <div className="cover"></div> */}
-                    <img className="cover" src={img}  />
+                    <img onClick={this.handlePlay} className="cover" src={img}  />
                     <div className="text">
                         <button>P</button>
                         <button>+</button>
@@ -67,13 +99,14 @@ export default class GenreListItem extends React.Component{
                     
                 </div>
             )
-        }else if(this.state.showModal && this.state.hovered){
+        } else if (this.state.showModal && this.state.hovered && !this.state.playVideo){
             return (
                 < MovieShowPageContainer video={this.props.video} hoveredExit={this.handleHoverExit} hovered={this.handleHover} showModal={this.showModal} />
             )
         } else if (!this.state.showModal && this.state.hovered){
             return(
-            <div onClick={this.showModal}
+            <div 
+                // onClick={this.showModal}
                 className="square"
                 onMouseEnter={this.handleHover}
                 onMouseLeave={this.handleHoverExit}>
@@ -88,13 +121,13 @@ export default class GenreListItem extends React.Component{
                 <div className="text">
                     <div className="movie-card-buttons">
                         <div className="right-btns">
-                        {/* <button className="play-card-btn">P</button> */}
                         <img  onClick={this.handlePlay} className="play-card-btn" src={playButton} alt="" />
-                                <img className="play-card-btn" src="https://img.icons8.com/ios/50/000000/plus-math.png" alt="" />
+                                {/* <img className="play-card-btn" src="https://img.icons8.com/ios/50/000000/plus-math.png" alt="" /> */}
+                                <button onClick={this.handleAddToList} className="movie-show-page-btn">{text}</button>
                         {/* <img className="add-to-list-btn">+</button>     */}
                         </div>
                         <div>
-                            <button className="movie-show-page-btn">-</button>
+                                <button onClick={this.showModal} className="movie-show-page-btn">-</button>
                         </div>
                     </div>
                     <div className="movie-card-genre-list">
@@ -107,7 +140,29 @@ export default class GenreListItem extends React.Component{
                 </div>
             </div>
             )
-        }
+        } 
+        // else if (!this.state.showModal && this.state.hovered && this.state.playVideo) {
+
+        // }
     }
 
 }
+
+const MapStateToProps = (state, ownProps) => {
+    return {
+        videos: Object.values(state.entities.videos),
+        lists: state.entities.myList,
+        currentProfile: state.session.currentProfile
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchAllVideos: videos => dispatch(fetchAllVideos(videos)),
+        createListItem: (movieId, profileId) => dispatch(createListItem(movieId, profileId)),
+        fetchlists: lists => dispatch(fetchlists(lists)),
+        deleteMylist: listId => dispatch(deleteMylist(listId))
+    }
+}
+
+export default connect(MapStateToProps, mapDispatchToProps)(GenreListItem)
